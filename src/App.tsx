4281dc8f215804330683584abcc51edb58889cb0
Nobuhiko FUNATO <nfunato@acm.org>
2022-05-27 17:21:58 +0900

    add: buggy version (around printing userid)

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
+import { useState } from "react";
 import axios from "axios";
 import "./styles.css";
+import { Todo } from "./Todo";
 
 export const App = () => {
+  const [todos, setTodos] = useState<any>([]);
   const onClickFetchData = () => {
     axios
       .get("https://jsonplaceholder.typicode.com/todos")
-      .then((res) => console.log(res));
+      .then((res) => setTodos(res.data));
   };
   return (
     <div className="App">
       <button onClick={onClickFetchData}>データ取得</button>
+      {todos.map((todo) => (
+        <Todo title={todo.title} userid={todo.userid} />
+      ))}
     </div>
   );
 };
