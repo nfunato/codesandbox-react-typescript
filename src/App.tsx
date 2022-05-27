117ca5a5cd6baf57b44327debfcfcc4917aaa1c1
Nobuhiko FUNATO <nfunato@acm.org>
2022-05-27 17:40:36 +0900

    fix bug in App by introducing type TodoType

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,28 @@ import axios from "axios";
 import "./styles.css";
 import { Todo } from "./Todo";
 
+type TodoType = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
 export const App = () => {
-  const [todos, setTodos] = useState<any>([]);
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
   const onClickFetchData = () => {
     axios
-      .get("https://jsonplaceholder.typicode.com/todos")
-      .then((res) => setTodos(res.data));
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        setTodos(res.data);
+      });
   };
   return (
     <div className="App">
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo) => (
-        <Todo title={todo.title} userid={todo.userid} />
+        // fix typo: userid -> userId
+        <Todo title={todo.title} userid={todo.userId} />
       ))}
     </div>
   );
